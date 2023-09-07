12d6005fbf5beff97d633e5f782252977c06a848
JohnIanOngayi <ongayijohnian@gmail.com>
2023-09-07 06:40:16 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - This is the main function
+ * Description: Function prints a string out
+ * It is a C source code
+ * Parameters: No arguments
+ * Return: A zero integer
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

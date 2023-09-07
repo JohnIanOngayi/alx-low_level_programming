d7820f77f5af1c592cb472ccfd07cc0b3112d868
JohnIanOngayi <ongayijohnian@gmail.com>
2023-09-07 07:00:32 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - This is the main function
+ * parameters: No arguments
+ * Description: Prints string to terminal
+ * Return: A zero integer
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

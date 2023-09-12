bcf0d400df9e0f025d01de4b25063f5a0a59ca7b
JohnIanOngayi <ongayijohnian@gmail.com>
2023-09-12 12:50:51 +0300

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * print_sign - checks for the signage of a number
+  * @n: integer beine checked
+  * Return: 1 if (+)ve
+  * if o -1 if (-)ve
+  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}

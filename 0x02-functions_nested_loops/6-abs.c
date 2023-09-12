d4acfd0c09fd9fa74cb3c68088d4844db4398d64
JohnIanOngayi <ongayijohnian@gmail.com>
2023-09-12 13:08:42 +0300

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * _abs - Computes absolute value of an integer
+  * @num: A random integer
+  * Return: 0 Always success
+  */
+int _abs(int num)
+{
+	if (num > 0)
+		return (num);
+	else
+		return (-num);
+}

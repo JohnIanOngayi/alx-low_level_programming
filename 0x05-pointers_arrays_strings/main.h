19fb6d7521369795f1739055f77839dafde7841b
JohnIanOngayi <ongayijohnian@gmail.com>
2023-09-18 14:24:36 +0300

    add: 4-print_rev.c
    Prints a string, in reverse, followed by a new line

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,5 +7,12 @@ void reset_to_98(int *n);
 void print_array(int *a, int n);
 void puts2(char *str);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts_half(char *str);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+void _putchar(char c);
 
 #endif /*MAIN_H*/

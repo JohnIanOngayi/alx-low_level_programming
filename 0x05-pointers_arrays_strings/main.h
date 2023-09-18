e607b010fb0503a4871423f5b6e3719dfbc26455
JohnIanOngayi <ongayijohnian@gmail.com>
2023-09-18 18:13:12 +0300

    add: 3-puts.c
    Prints a string, followed by a new line, to stdout

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ void print_array(int *a, int n);
 void puts2(char *str);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts_half(char *str);

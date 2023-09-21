8e673c40f7f7efb89e40911874089373db1c0447
JohnIanOngayi <ongayijohnian@gmail.com>
2023-09-21 16:52:04 +0300

    feat: 1-strncat.c
    function that implements strncat()

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  * _strncat - conactenates two strings
+  *@dest: the main string
+  * @src: thestring being concatenated onto dest
+  * @n: number of characters to be attached
+  * Return: A pointer to dest
+  */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}

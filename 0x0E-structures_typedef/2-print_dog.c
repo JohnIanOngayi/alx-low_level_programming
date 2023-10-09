7c1f68be08c868e8bbce346060bf69a41bd0544a
JohnIanOngayi <ongayijohnian@gmail.com>
2023-10-09 16:41:40 +0300

    [Task] 2-print_dog.c
    Adding a function that prints a struct dog.
    Prototype: void print_dog(struct dog *d);

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - prints a struct dog
+ * @d: a pointer to struct dog
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	printf("Name: ");
+	if (d->name == NULL)
+		printf("(nil)\n");
+	else
+		printf("%s\n", d->name);
+
+	printf("Age: %f\n", d->age);
+
+	printf("Owner: ");
+	if (d->owner == NULL)
+		printf("(nil)\n");
+	else
+		printf("%s\n", d->owner);
+}

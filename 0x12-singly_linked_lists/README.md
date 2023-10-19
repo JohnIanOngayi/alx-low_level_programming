ae0ce6ca3d09b6043e4c0057b2dfbd33630adff2
JohnIanOngayi <ongayijohnian@gmail.com>
2023-10-19 20:44:59 +0300

    [Task] 0-print_list.c
    Adding a function that prints all the elements of a list

---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,8 @@
+# Singly Linked Lists
+
+This repository contains programs that illustrate the elementary applications and functionality of linked lists data structure.
+
+## Tasks
+
+### Task 1
+
---
+++ 0x12-singly_linked_lists/_strlen.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * _strlen - Returns the length of a string.
+ * @s: A pointer to a character string
+ * Return: The length of the string.
+ */
+int _strlen(const char *s)
+{
+	unsigned int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+	return (len);
+
+}
